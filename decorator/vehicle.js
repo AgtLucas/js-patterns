d20f47b3920c669e27150159cdf52fa65e67e78e
Lucas <lucas.silva@agenciamilagro.com.br>
2014-05-08 11:00:27 -0300

    Update vehicle.js

--- decorator/vehicle.js
+++ decorator/vehicle.js
@@ -1,6 +1,26 @@
 // A vehicle constructor
 function Vehicle(vehicleType) {
 
-  'use strict';
+  this.vehicleType = vehicleType || 'car';
+  this.model = 'default';
+  this.license = '0000-000';
 
 }
+
+// Test instance for a basic vehicle
+var testInstance = new Vehicle('car');
+console.log(testInstance);
+
+// Let's create a new instance of vehicle, to be decorated
+var truck = new Vehicle('truck');
+
+truck.setModel = function(modelName) {
+  this.modelName = modelName;
+};
+
+truck.setColor = function(color) {
+  this.color = color;
+};
+
+truck.setModel('CAT');
+truck.setColor('Black');
