030e25a9e3c85e31ca34a297c4bec388844ca6c7
Lucas Churchill <lucas@agtlucas.com>
2014-05-08 22:05:15 -0300

    Update factory

--- factory/factory.js
+++ factory/factory.js
@@ -3,4 +3,28 @@ function Car(options) {
 
   'use strict';
 
+  this.doors = options.doors || 4;
+  this.state = options.state || 'Brand new';
+  this.color = options.color || 'Black';
+
+}
+
+// Define a skeleton vehicle factory
+function VehicleFactory() {}
+
+VehicleFactory.prototype.vehicleClass = Car;
+
+VehicleFactory.prototype.createNewVehicle = function (options) {
+
+  switch (options.vehicleType) {
+      case 'car':
+        this.vehicleClass = Car;
+        break;
+      case 'truck':
+        this.vehicleClass = Truck;
+        break;
+  }
+
+  return new this.vehicleClass(options);
+
 }
