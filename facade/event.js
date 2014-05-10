d20641cc1fe5f3a0e285da6bd1f7d5b7c24d0dac
Lucas Churchill <lucas@agtlucas.com>
2014-05-09 22:51:43 -0300

    Update event.js on facade

---
+++ facade/event.js
@@ -0,0 +1,17 @@
+(function () {
+
+  'use strict';
+
+  var addMyEvent = function (el, ev, fn) {
+
+    if (el.addEventListener) {
+      el.addEventListener(ev, fn, false);
+    } else if (el.attachEvent) {
+      el.attachEvent('on' + ev, fn);
+    } else {
+      el['on' + ev] = fn;
+    }
+
+  };
+
+}());
